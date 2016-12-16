ac1632878278b7ddc286964df326ceeed15d0878
darkenvy <darkenvy6@gmail.com>
2016-12-16 10:58:16 -0800

    need to 'flatten' async functions for montage to work properly

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 var gm = require('gm').subClass({imageMagick: true});
 // import/default/
-var path = 'assets/minecraft/textures/blocks/'
+// var insidePath = ''
+// blocks/
 var blockSprites = require('./block-sprites')
-// var importDirectory = await findImportDirectory()
-console.log("AWAITED", await findImportDirectory());
+var importDirectory = findImportDirectory(); // Declare so find only runs once
 
 // ----------------- Create Output Directory Layout ----------------- //
 
@@ -31,30 +31,48 @@ function makeLayout(folder) {
 // ----------------- Helper Functions ----------------- //
 
 function findImportDirectory() {
+  console.log('running findImportDirectory :\\');
   // Returns a promise with the most likely folder to be importing
   var dir = new Promise((resolve, reject) => {
     fs.readdir(__dirname + '/import/', (err, list) => resolve(list))
   });
-
   return dir.then(list => {
     var mostLikely = 'default';
     list.forEach(i=>{ // Cycle through each folder. If not hidden or 'default'
       if (i !== 'default' && i[0] !== '.') {mostLikely = i}
-    })
+    });
     return mostLikely;
+  });
+}
+
+function findPNG(shortDirectory) {
+  // Returns either the imported texturepack path or the default path
+  return importDirectory.then(dir => {
+    let userDir = __dirname + '/import/' + dir + '/assets/minecraft/textures/' + shortDirectory;
+    let defaultDir = __dirname + '/import/default/assets/minecraft/textures/' + shortDirectory;
+    // if (fs.existsSync(userDir)) {
+    if (false) {
+      return userDir;
+    } else if (fs.existsSync(defaultDir)) {
+      return defaultDir;
+    } else {
+      console.log("!!!ERROR!!! Report this filename: \"", shortDirectory, "\"");
+      return __dirname + '/import/default/404.png';
+    }
   })
 
 }
-findImportDirectory().then(i=>console.log(i))
 
-function checkForPNG() {}
+// findPNG('blocks/gravel.png').then(i=>console.log(i))
 
 // ----------------- Create Terrain Spritesheet ----------------- //
 
 spritesheet = gm(256,512).background('transparent');
 
 blockSprites.forEach((sprite) => {
-  spritesheet.montage( __dirname + '/import/default/' + path + sprite);
+  // console.log('here:','blocks/' + sprite);
+  // spritesheet.montage(dir)
+  // findPNG('blocks/' + sprite).then(dir => )
 })
 
 spritesheet
@@ -64,15 +82,15 @@ spritesheet
     else {console.log(err);}
   })
   
-spritesheet
-  .geometry('8x8+0+0').tile('16x32')
-  .write('export/terrainMipMapLevel2.png', (err) => {
-    if (!err) {console.log('written terrainMipMapLevel2')} 
-  })
+// spritesheet
+//   .geometry('8x8+0+0').tile('16x32')
+//   .write('export/terrainMipMapLevel2.png', (err) => {
+//     if (!err) {console.log('written terrainMipMapLevel2')} 
+//   })
 
-spritesheet
-  .geometry('4x4+0+0').tile('16x32')
-  .write('export/terrainMipMapLevel3.png', (err) => {
-    if (!err) {console.log('written terrainMipMapLevel3')} 
-  })
+// spritesheet
+//   .geometry('4x4+0+0').tile('16x32')
+//   .write('export/terrainMipMapLevel3.png', (err) => {
+//     if (!err) {console.log('written terrainMipMapLevel3')} 
+//   })
 
