970ea2bd4e77e20b313b72c62f7c72a9461fbe59
darkenvy <darkenvy6@gmail.com>
2016-12-16 10:11:16 -0800

    about to try await.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var fs = require('fs');
 var gm = require('gm').subClass({imageMagick: true});
-var defaultPath = 'import/default/assets/minecraft/textures/blocks/'
+// import/default/
+var path = 'assets/minecraft/textures/blocks/'
 var blockSprites = require('./block-sprites')
+// var importDirectory = await findImportDirectory()
+console.log("AWAITED", await findImportDirectory());
 
 // ----------------- Create Output Directory Layout ----------------- //
 
@@ -14,30 +17,44 @@ var layout = [
   '/export/mob',
   '/export/textures'
 ]
+
 layout.forEach((i)=>{
   makeLayout(__dirname + i);
 })
 
 function makeLayout(folder) {
-  fs.exists(folder, (exists) => {
-    if (!exists) {
-      fs.mkdir(folder, ()=>{console.log('created dir');})
-    } else {
-      console.log('no need to create dir');
-    }
-  });
+  if (!fs.existsSync(folder)) {
+    fs.mkdir(folder, ()=>console.log('created dir'));
+  }
 }
 
-// -----------------  ----------------- //
+// ----------------- Helper Functions ----------------- //
 
+function findImportDirectory() {
+  // Returns a promise with the most likely folder to be importing
+  var dir = new Promise((resolve, reject) => {
+    fs.readdir(__dirname + '/import/', (err, list) => resolve(list))
+  });
 
+  return dir.then(list => {
+    var mostLikely = 'default';
+    list.forEach(i=>{ // Cycle through each folder. If not hidden or 'default'
+      if (i !== 'default' && i[0] !== '.') {mostLikely = i}
+    })
+    return mostLikely;
+  })
+
+}
+findImportDirectory().then(i=>console.log(i))
+
+function checkForPNG() {}
 
 // ----------------- Create Terrain Spritesheet ----------------- //
 
 spritesheet = gm(256,512).background('transparent');
 
 blockSprites.forEach((sprite) => {
-  spritesheet.montage(defaultPath + sprite);
+  spritesheet.montage( __dirname + '/import/default/' + path + sprite);
 })
 
 spritesheet
